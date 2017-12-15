91400822f9394dd76f565756c7ffee2a65c47d36
Funtrac <funbe23@gmail.com>
2017-12-15 16:36:11 +0100

    Update main.c

--- main.c
+++ main.c
@@ -11,10 +11,11 @@ typedef struct{
 	int x;
 	int y;
 } coord ;
+
 void clearboard(){
 	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 }
-void displayintro() {
+void displaylogo(){
 	printf("------------------------------------------------------------------------------\n");
 	printf("  ____    _       _                     ____    _                             \n");
 	printf(" / ___|  | |__   (_)  _ __     __ _    / ___|  | |__     __ _   _ __     __ _ \n");
@@ -24,71 +25,118 @@ void displayintro() {
 	printf("                              |___/                                     |___/ \n");
 	printf("------------------------------------------------------------------------------\n");
 	printf("\n\n");
+}
+void displaycurrent(int joueur,char msg[]) {
+	if (msg != " ") {
+		printf("%s\n",msg);
+	}
+	else{
+		printf("                          C'est à Joueur %d de jouer                          \n", joueur);
+	}
+	printf("\n");
+}
+void displayfooter(){
+	printf("\n& : quitter |  ←↕→ : se déplacer  |  Entrée (↵) : sélectionner une case  \n");
+}
+void displayconf(){
+	printf("\nq : quitter sans sauvegarder |  w : sauvegarder et quitter | ! : annuler \n");
+}
+void displayintro() {
+	printf("------------------------------------------------------------------------------\n");
+	printf("  ____    _       _                     ____    _                             \n");
+	printf(" / ___|  | |__   (_)  _ __     __ _    / ___|  | |__     __ _   _ __     __ _ \n");
+	printf(" \\___ \\  | '_ \\  | | | '_ \\   / _` |   \\___ \\  | '_ \\   / _` | | '_ \\   / _` |\n");
+	printf("  ___) | | | | | | | | | | | | (_| |    ___) | | | | | | (_| | | | | | | (_| |\n");
+	printf(" |____/  |_| |_| |_| |_| |_|  \\__, |   |____/  |_| |_|  \\__,_| |_| |_|  \\__, |\n");
+	printf("                              |___/                                     |___/ \n");
+	printf("______________________________________________________________________________\n");
+	printf("\n\n");
 	printf("                          DUVAL Lucas - MEURDRAC Téo                          \n");
 	printf("                       2017 - Université Caen Normandie                       \n");
 	printf("\n\n\n\n\n\n");
 	printf("                           ───────────────────────                            \n");
 	printf("                           Press Enter to Continue                            \n");
 	printf("                           ───────────────────────                            \n");
-	printf("\n\n\n\n");
+	printf("\n\n");
 }
-void deplacable(coord coordonnees, coord * autorise[][10], coord * plateau[][10]){
-	int varx,vary,tmpx,tmpy;
-	for (int i=0; i <3 ; i++){
-		for (int j=0; j<3; j++){
-			varx=coordonnees.x-1+i;
-			vary=coordonnees.y-1+i;
-			if (varx<0){
-				varx=0;
-			}
-			if (vary<0){
-				varx=0;
-			}
-			if (plateau[varx][vary]==NULL) {
-				autorise[varx][vary]=1;
-			}
-			else {
-				autorise[varx][vary]=0;
-				difx=varx-coordonnees.x;
-				dify=vary-coordonnees.y;
-				if (plateau[varx+difx][vary+dify]==NULL) {
-					autorise[varx+difx][vary+dify]=1;
+
+int abs(int x){
+	if (x <0 ){
+		x=-x;
+	}
+	return x;
+}
+
+int movesinge(pion * plateau[][10],coord movefrom,coord moveto, int isplaying){
+	int val = 0;
+	coord coordcheck;
+	if(abs(movefrom.x-moveto.x) <=2 && abs(movefrom.y-moveto.y) <= 2){
+		printf("Working\n");
+		//test si déplacement de 1 ou de 2
+		if(abs(movefrom.x-moveto.x) == 2 || abs(movefrom.y-moveto.y) == 2){
+			//si déplacement de 2 alors
+			printf("Dep 2\n");
+			//test si coord = une case du carré 5x5 impossible a atteindre
+			if(!(abs(movefrom.x-moveto.x) == 2 && abs(movefrom.y-moveto.y)==1)  || (!(abs(movefrom.x-moveto.x) == 1 && abs(movefrom.y-moveto.y)==2))){
+				//calcul de la case "de passage"
+				coordcheck.x = movefrom.x+(moveto.x-movefrom.x)/2;
+				coordcheck.y = movefrom.y+(moveto.y-movefrom.y)/2;
+				if(plateau[coordcheck.x][coordcheck.y] == NULL){
+					plateau[moveto.x][moveto.y] = plateau[movefrom.x][movefrom.y];
+					plateau[movefrom.x][movefrom.y] = NULL;
+					val = 1; //return normal move value
 				}
-				else {
-					autorise[varx+difx][vary+dify]=0;
+				else{
+					if(plateau[coordcheck.x][coordcheck.y]->team == isplaying && plateau[coordcheck.x][coordcheck.y]->type <= plateau[movefrom.x][movefrom.y]->type){
+						//if same team && type1>=type2
+						plateau[moveto.x][moveto.y] = plateau[movefrom.x][movefrom.y];
+						plateau[movefrom.x][movefrom.y] = NULL;
+						val = 2;
+					}
+					else if(plateau[coordcheck.x][coordcheck.y]->type <= plateau[movefrom.x][movefrom.y]->type){
+						//If ennemy team && type1>=type2
+						plateau[moveto.x][moveto.y] = plateau[movefrom.x][movefrom.y];
+						plateau[movefrom.x][movefrom.y] = NULL;
+						plateau[coordcheck.x][coordcheck.y] = NULL;
+						val = 2; //return jump value
+					}
 				}
 			}
 		}
+		else{
+			//si déplacement de 1
+			printf("Dep 1\n");
+			if(plateau[moveto.x][moveto.y] == NULL){//test si occupé
+				plateau[moveto.x][moveto.y] = plateau[movefrom.x][movefrom.y];
+				plateau[movefrom.x][movefrom.y] = NULL;
+				val = 1;//return normal move value
+			}
+		}
 	}
+	printf("val = %d\n",val);
+	return val;
 }
-
-void deplacement_singe(coord cordonnees,  coord * plateau[][10], coord movefrom, coord moveto){
-	coord * autorise[10][10];
-	deplacable(movefrom, autorise,plateau)
-	printf("")
-}
-int requestmove(pion * plateau[][10],coord * movefrom,coord * moveto){
+int requestmove(pion * plateau[][10],coord movefrom,coord moveto,int isplaying){
 	int val = 0;
-	switch (plateau[movefrom->x][movefrom->y]->type) {
+	switch (plateau[movefrom.x][movefrom.y]->type) {
 		case 1:
-				val = deplacement_singe(plateau,*(movefrom),*(moveto));
+		printf("Requested\n");
+		val = movesinge(plateau,movefrom,moveto,isplaying);
 		break;
 		case 2:
-				val = deplacement_lion(plateau,*(movefrom),*(moveto));
+		//val = movelion(plateau,movefrom,moveto,isplaying);
 		break;
 		case 3:
-			val = deplacement_dragon(plateau,*(movefrom),*(moveto));
+		//val = movedragon(plateau,movefrom,moveto,isplaying);
 		break;
 	}
-	movefrom->x = -1;
-	movefrom->y = -1;
 	return val;
 }
 void nextplayer(int * isp){
 	if (*isp==1)
-		*isp=2;
+	*isp=2;
 	else
-		*isp=1;
+	*isp=1;
 }
 void affichecase(int team, int type,int colored){
 	char * tabaff[3][5];
@@ -98,37 +146,42 @@ void affichecase(int team, int type,int colored){
 	tabaff[1][1]="◇";
 	tabaff[1][2]="△";
 	tabaff[1][3]="○";
-  tabaff[1][4]="P";
+	tabaff[1][4]="P";
 
 	tabaff[2][1]="◆";
 	tabaff[2][2]="▲";
 	tabaff[2][3]="●";
-  tabaff[2][4]="P";
+	tabaff[2][4]="P";
 	switch (colored) {
 		case 2 :
-			printf("\x1b[31m%s\x1b[0m ",tabaff[team][type]);
+		printf("\x1b[31m%s\x1b[0m ",tabaff[team][type]);
 		break;
 		case 1 :
-			printf("\x1b[33m%s\x1b[0m ",tabaff[team][type]);
+		printf("\x1b[33m%s\x1b[0m ",tabaff[team][type]);
 		break;
 		default:
-			printf("%s ",tabaff[team][type]);
+		printf("%s ",tabaff[team][type]);
 		break;
 	}
 }
 
-int affichageplateau(pion * plateau[][10],coord focused, coord selected){
+void affichageplateau(pion * plateau[][10],coord focused, coord selected,int isplaying,char * msg){
 	int testtype, testteam;
+	displaylogo();
+	displaycurrent(isplaying,msg);
 	for (int i=0; i<10; i++){
+		for (int k = 0; k < 28; k++) {
+			printf(" ");
+		}
 		for (int j=0; j<10; j++){
-      if (plateau[j][i] != NULL) {
-        testtype=plateau[j][i]->type;
-  			testteam=plateau[j][i]->team;
-      }
-      else{
-        testtype = 0;
-        testteam = 0;
-      }
+			if (plateau[j][i] != NULL) {
+				testtype=plateau[j][i]->type;
+				testteam=plateau[j][i]->team;
+			}
+			else{
+				testtype = 0;
+				testteam = 0;
+			}
 			if (j == selected.x && i == selected.y) {
 
 				affichecase(testteam,testtype,2);
@@ -140,230 +193,276 @@ int affichageplateau(pion * plateau[][10],coord focused, coord selected){
 				affichecase(testteam,testtype,0);
 			}
 		}
-    printf("\n");
+		printf("\n");
 	}
+	displayfooter();
 }
 
 
-coord * generetable(pion * plateau[][10], pion * tcase){
-  //Case Inaccessible
-  (tcase)->type = 1;
-  (tcase)->team = 0;
-  //Pions Blancs et portail blanc
-  (tcase+1)->type = 1;
-  (tcase+1)->team = 1;
+void generetable(pion * plateau[][10], pion * tcase){
+	//Case Inaccessible
+	(tcase)->type = 1;
+	(tcase)->team = 0;
+	//Pions Blancs et portail blanc
+	(tcase+1)->type = 1;
+	(tcase+1)->team = 1;
 
-  (tcase+2)->type = 2;
-  (tcase+2)->team = 1;
+	(tcase+2)->type = 2;
+	(tcase+2)->team = 1;
 
-  (tcase+3)->type = 3;
-  (tcase+3)->team = 1;
+	(tcase+3)->type = 3;
+	(tcase+3)->team = 1;
 
-  (tcase+4)->type = 4;
-  (tcase+4)->team = 1;
+	(tcase+4)->type = 4;
+	(tcase+4)->team = 1;
+	//Pions Noirs et portail noir
+	(tcase+5)->type = 1;
+	(tcase+5)->team = 2;
 
-  //Pions Noirs et portail noir
-  (tcase+5)->type = 1;
-  (tcase+5)->team = 2;
+	(tcase+6)->type = 2;
+	(tcase+6)->team = 2;
 
-  (tcase+6)->type = 2;
-  (tcase+6)->team = 2;
+	(tcase+7)->type = 3;
+	(tcase+7)->team = 2;
 
-  (tcase+7)->type = 3;
-  (tcase+7)->team = 2;
+	(tcase+8)->type = 4;
+	(tcase+8)->team = 2;
+	//Initialisation du tableau à la valeur NULL
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			plateau[i][j] = NULL;
+		}
+	}
+	//Portails Blancs
+	plateau[4][1] = tcase+4;
+	plateau[5][1] = tcase+4;
+	//Portails Noirs
+	plateau[4][8] = tcase+8;
+	plateau[5][8] = tcase+8;
 
-  (tcase+8)->type = 4;
-  (tcase+8)->team = 2;
-  //Initialisation du tableau à la valeur NULL
-  for (int i = 0; i < 10; i++) {
-    for (int j = 0; j < 10; j++) {
-      plateau[i][j] = NULL;
-    }
-  }
-  //Triangle 1
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4-i; j++) {
-      if (j==0) {
-        plateau[j][i] = tcase;
-      }
-      else if((3-j)==i){
-        plateau[j][i] = tcase+1;
-      }
-      else if (i==(j-1)) {
+	//Triangle 1
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4-i; j++) {
+			if (j==0) {
+				plateau[j][i] = tcase;
+			}
+			else if((3-j)==i){
+				plateau[j][i] = tcase+1;
+			}
+			else if (i==(j-1)) {
 				plateau[j][i] = tcase+3;
-      }
-      else{
+			}
+			else{
 				plateau[j][i] = tcase+2;
-      }
-    }
-  }
-  //Triangle 2
-  for (int i = 0; i < 4; i++) {
-    for (int j = 9; j > 5+i; j--) {
-      if (j==9) {
-        plateau[j][i] = tcase;
-      }
-      else if(i==(j-6)){
-        plateau[j][i] = tcase+1;
-      }
-      else if (i==(j-7)) {
-        plateau[j][i] = tcase+2;
-      }
-      else{
-        plateau[j][i] = tcase+3;
-      }
-    }
-  }
-  //Triangle 3
-  for (int i = 6; i < 10; i++) {
-    for (int j = 0; j < i-5; j++) {
-      if (j==0) {
-        plateau[j][i] = tcase;
-      }
-      else if((i-6)==j){
-        plateau[j][i] = tcase+5;
-      }
-      else if ((i-7)==j) {
-        plateau[j][i] = tcase+6;
-      }
-      else{
-        plateau[j][i] = tcase+7;
-      }
-    }
-  }
-  //Triangle 4
-  for (int i = 6; i < 10; i++) {
-    for (int j = 9; j > 8+6-i; j--) {
-      if (j==9) {
-        plateau[j][i] = tcase;
-      }
-      else if(i==(15-j)){
-        plateau[j][i] = tcase+5;
-      }
-      else if (i==(16-j)) {
-        plateau[j][i] = tcase+6;
-      }
-      else{
-        plateau[j][i] = tcase+7;
-      }
-    }
-  }
+			}
+		}
+	}
+	//Triangle 2
+	for (int i = 0; i < 4; i++) {
+		for (int j = 9; j > 5+i; j--) {
+			if (j==9) {
+				plateau[j][i] = tcase;
+			}
+			else if(i==(j-6)){
+				plateau[j][i] = tcase+1;
+			}
+			else if (i==(j-7)) {
+				plateau[j][i] = tcase+2;
+			}
+			else{
+				plateau[j][i] = tcase+3;
+			}
+		}
+	}
+	//Triangle 3
+	for (int i = 6; i < 10; i++) {
+		for (int j = 0; j < i-5; j++) {
+			if (j==0) {
+				plateau[j][i] = tcase;
+			}
+			else if((i-6)==j){
+				plateau[j][i] = tcase+5;
+			}
+			else if ((i-7)==j) {
+				plateau[j][i] = tcase+6;
+			}
+			else{
+				plateau[j][i] = tcase+7;
+			}
+		}
+	}
+	//Triangle 4
+	for (int i = 6; i < 10; i++) {
+		for (int j = 9; j > 8+6-i; j--) {
+			if (j==9) {
+				plateau[j][i] = tcase;
+			}
+			else if(i==(15-j)){
+				plateau[j][i] = tcase+5;
+			}
+			else if (i==(16-j)) {
+				plateau[j][i] = tcase+6;
+			}
+			else{
+				plateau[j][i] = tcase+7;
+			}
+		}
+	}
 }
 
 
 int main(){
+	int g,val;
+	int isplaying = 1;
+	int brek = 0;
+	char * msg = "";
+	static struct termios oldt, newt;
 
-int g,val;
-static struct termios oldt, newt;
+	/*tcgetattr gets the parameters of the current terminal
+	STDIN_FILENO will tell tcgetattr that it should write the settings
+	of stdin to oldt*/
+	tcgetattr( STDIN_FILENO, &oldt);
+	/*now the settings will be copied*/
+	newt = oldt;
 
-/*tcgetattr gets the parameters of the current terminal
-STDIN_FILENO will tell tcgetattr that it should write the settings
-of stdin to oldt*/
-tcgetattr( STDIN_FILENO, &oldt);
-/*now the settings will be copied*/
-newt = oldt;
+	/*ICANON normally takes care that one line at a time will be processed
+	that means it will-1 return if it sees a "\n" or an EOF or an EOL*/
+	newt.c_lflag &= ~(ICANON);
 
-/*ICANON normally takes care that one line at a time will be processed
-that means it will return if it sees a "\n" or an EOF or an EOL*/
-newt.c_lflag &= ~(ICANON);
+	/*Those new settings will be set to STDIN
+	TCSANOW tells tcsetattr to change attributes immediately. */
+	tcsetattr( STDIN_FILENO, TCSANOW, &newt);
 
-/*Those new settings will be set to STDIN
-TCSANOW tells tcsetattr to change attributes immediately. */
-tcsetattr( STDIN_FILENO, TCSANOW, &newt);
-
-clearboard();
-displayintro();
-
-while (getchar()!= '\n') {
 	clearboard();
 	displayintro();
-}
-//Initialisation du tableau de pointeurs
-pion * plateau[10][10];
-//Initialisation du tableau de type de cases
-pion * tcase = (pion *) malloc(9*sizeof(coord)); // À EXPLIQUER
-clearboard();
-generetable(plateau,tcase);
 
-coord coordselect;
-coordselect.x = -1;
-coordselect.y = -1;
-
-coord focused;
-focused.x = 0;
-focused.y = 0;
-
-affichageplateau(plateau,focused,coordselect);
-
-int isplaying = 1;
-
-
-while((g=getchar())!= '&'){
-	clearboard();
-	if (g == '\033') {
-		if (getchar()) {
-			switch (getchar()) {
-				case 'A':
-				if (focused.y > 0) {
-					focused.y -= 1;
-				}
-				break;
-				case 'B':
-				if (focused.y < 9) {
-					focused.y += 1;
-				}
-				break;
-				case 'C':
-				if (focused.x < 9) {
-					focused.x += 1;
-				}
-				break;
-				case 'D':
-				if (focused.x > 0) {
-					focused.x -= 1;
-				}
-				break;
-			}
-		}
+	while (getchar()!= '\n') {
+		clearboard();
+		displayintro();
 	}
-	else if(g=='\n'){
-		if (coordselect.x != -1 && coordselect.y != -1) {
-			//val = requestmove(plateau,&coordselect,&focused);
-			if (val == 1) {
-				nextplayer(&isplaying);
-			}
-			else if (!val) {
-				printf("Déplacement impossible !\n");
+	//Initialisation du tableau de pointeurs
+	pion * plateau[10][10];
+	//Initialisation du tableau de tmsgype de cases
+	pion * tcase = (pion *) malloc(9*sizeof(coord)); // À EXPLIQUER
+	clearboard();
+	generetable(plateau,tcase);
+
+	coord coordselect;
+	coordselect.x = -1;
+	coordselect.y = -1;
+
+	coord focused;
+	focused.x = 0;
+	focused.y = 0;
+
+	affichageplateau(plateau,focused,coordselect,isplaying,"");
+
+
+	while((g=getchar()) && brek != 1){
+		clearboard();
+		if (g == '\033') {
+			if (getchar()) {
+				switch (getchar()) {
+					case 'A':
+					if (focused.y > 0) {
+						focused.y -= 1;
+					}
+					break;
+					case 'B':
+					if (focused.y < 9) {
+						focused.y += 1;
+					}
+					break;
+					case 'C':
+					if (focused.x < 9) {
+						focused.x += 1;
+					}
+					break;
+					case 'D':
+					if (focused.x > 0) {
+						focused.x -= 1;
+					}
+					break;
+				}
 			}
 		}
-		else{
-			if (plateau[focused.x][focused.y]!=NULL) {
-				if (plateau[focused.x][focused.y]->team == isplaying) {
-					coordselect = focused;
-					printf("Sélectionné !\n");
+		else if(g=='\n'){
+			if (coordselect.x != -1 && coordselect.y != -1 && (plateau[focused.x][focused.y] == NULL || plateau[focused.x][focused.y]->type == 4)) {
+				val = requestmove(plateau,coordselect,focused,isplaying);
+				if (val == 1) {
+					nextplayer(&isplaying);
+					msg = "";
+					coordselect.x = -1;
+					coordselect.y = -1;
 				}
-				else if (!plateau[focused.x][focused.y]->team) {//team 0 = case maudite
-					printf("Vous ne pouvez pas sélectionner une case maudite !\n");
+				else if(val == 2){
+					msg = "                Voulez-vous continuer a jouer ? (O/N)                    ";
+					while(g != 'o' && g != 'O' && g != 'n' && g != 'N'){
+						affichageplateau(plateau,focused,coordselect,isplaying,msg);
+						g = getchar();
+					}
+					if (g == 'n' && g == 'N') {
+						nextplayer(&isplaying);
+						coordselect.x = -1;
+						coordselect.y = -1;
+						msg = " ";
+					}
+					else{
+						msg = " ";
+					}
 				}
-				else if (plateau[focused.x][focused.y]->type == 4) {//type 4 = portail
-					printf("Vous ne pouvez pas déplacer un portail !\n");
-				}
-				else{
-					printf("Vous ne pouvez pas sélectionner un pion ennemi !\n");
+				else if (!val) {
+					msg = "                          Déplacement impossible !                              ";
 				}
 			}
 			else{
-				printf("Vous ne pouvez pas sélectionner une case vide\n");
+				if (plateau[focused.x][focused.y]!=NULL) {
+					if (plateau[focused.x][focused.y]->team == isplaying && plateau[focused.x][focused.y]->type != 4) {
+						coordselect = focused;
+						msg = "                               Sélectionné !                                  ";
+					}
+					else if (!plateau[focused.x][focused.y]->team) {//team 0 = case maudite
+						msg = "             Vous ne pouvez pas sélectionner une case maudite !               ";
+
+					}
+					else if (plateau[focused.x][focused.y]->type == 4) {//type 4 = portail
+						msg = "                  Vous ne pouvez pas déplacer un portail !                    ";
+					}
+					else{
+						msg = "              Vous ne pouvez pas sélectionner un pion ennemi !                ";
+					}
+				}
+				else{
+					msg = "                 Vous ne pouvez pas sélectionner une case vide                  ";
+				}
 			}
 		}
+		affichageplateau(plateau,focused,coordselect,isplaying,msg);
+		if (g == '&') {
+			do {
+				if (g == 'w' || g == 'W') {
+					val = -1;
+					brek = 1;
+				}
+				else if (g == 'q' || g == 'Q') {
+					brek = 1;
+					val = -1;
+				}
+				else if(g == '!'){
+					val = -1;
+				}
+				clearboard();
+				displaylogo();
+				displayconf();
+			}
+			while (val != -1 && (g=getchar()));
+		}
 	}
-		affichageplateau(plateau,focused,coordselect);
-}
 
 
-/*restore the old settings*/
-tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
-free(tcase);
-clearboard();
-return 1;
+	/*restore the old settings*/
+	tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
+	free(tcase);
+	clearboard();
+	return 1;
 }
